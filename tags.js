e321257c07e5225cb17eabf5651c4bd241306e40
Nova2ezz <185642674+Nova2ezz@users.noreply.github.com>
2026-04-10 09:21:46 +0200

    Create tags.js

---
+++ tags.js
@@ -0,0 +1,11 @@
+{
+  "users": [
+    {
+      "userId": 123456789,
+      "name": "Zxc_Auraic",
+      "tag": "HITTA Zxc_Auraic",
+      "image": "https://raw.githubusercontent.com/Nova2ezz/images/main/1775775002486_x4.png",
+      "colors": ["#B39DDB", "#E3F2FD"]
+    }
+  ]
+}
