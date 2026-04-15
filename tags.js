daf1844d3b14deffc33e3a34a8e1ee9ca9825007
Nova2ezz <185642674+Nova2ezz@users.noreply.github.com>
2026-04-15 10:08:16 +0200

    Update tags.js

--- tags.js
+++ tags.js
@@ -1,7 +1,7 @@
 {
   "users": [
     {
-      "userId": 10454280901,
+      "userIds": [10454280901, 987654321],
       "name": "Zxc_Auraic",
       "tag": "HITTA Zxc_Auraic",
       "image": "https://raw.githubusercontent.com/Nova2ezz/images/main/1775775002486_x4.png",
